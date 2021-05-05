daee87fea9cc402aaea6655ec8f2f54e93d43e62
alceray <39638378+alceray@users.noreply.github.com>
2021-05-05 15:57:44 -0400

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -3,13 +3,13 @@ import random
 from settings import *
 
 class Block(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,count):
         pg.sprite.Sprite.__init__(self)
         self.surf = pg.Surface((TILE_SIZE,TILE_SIZE))
         self.surf.fill(random.choice(BLOCK_COLOURS))
         self.rect = self.surf.get_rect(
             center=(random.randint(0,9)*TILE_SIZE+TILE_SIZE/2,0))
-        self.speed = BLOCK_SPEED
+        self.speed = B_START_SPEED + (count//B_SPEED_INCREMENT) * B_SPEED_UP
     def update(self):
         self.rect.move_ip(0,self.speed)
         if self.rect.right > WIDTH:
@@ -27,14 +27,15 @@ class Tetris:
         self.screen = pg.display.set_mode(SCREEN_SIZE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(200,100)
+        self.count = 0
     def draw_grid(self):
         for x in range(0,WIDTH,TILE_SIZE):
-            pg.draw.line(self.screen,DARKGREY,(x,0),(x,HEIGHT))
+            pg.draw.line(self.screen,DARK_GREY,(x,0),(x,HEIGHT))
         for y in range(0,HEIGHT,TILE_SIZE):
-            pg.draw.line(self.screen,DARKGREY,(0,y),(WIDTH,y))
+            pg.draw.line(self.screen,DARK_GREY,(0,y),(WIDTH,y))
     def run(self):
         self.all_sprites = pg.sprite.Group()
-        self.block = Block()
+        self.block = Block(self.count)
         self.all_sprites.add(self.block)
         self.running = True
         while self.running:
@@ -56,7 +57,8 @@ class Tetris:
     def update(self):
         add_block = self.block.update()
         if add_block:
-            self.block = Block()
+            self.count += 1
+            self.block = Block(self.count)
             self.all_sprites.add(self.block)
     def draw(self):
         self.screen.fill(BG_COLOUR)
