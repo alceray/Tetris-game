256c7eebcc1c9edba232f57a929f4bc15eb7e683
alceray <39638378+alceray@users.noreply.github.com>
2021-05-06 02:43:55 -0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -8,9 +8,9 @@ class Block(pg.sprite.Sprite):
         self.surf = pg.Surface((TILE_SIZE,TILE_SIZE))
         self.surf.fill(random.choice(BLOCK_COLOURS))
         self.rect = self.surf.get_rect(
-            center=(random.randint(0,9)*TILE_SIZE+TILE_SIZE/2,0))
+            center=((0.5+random.randint(0,GRID_WIDTH-1))*TILE_SIZE,0))
         self.speed = B_START_SPEED + (count//B_SPEED_INCREMENT) * B_SPEED_UP
-    def update(self):
+    def update(self,group):
         self.rect.move_ip(0,self.speed)
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -19,15 +19,32 @@ class Block(pg.sprite.Sprite):
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
             return True
+        for sprite in group:
+            if sprite != self and pg.sprite.collide_rect(self,sprite):
+                self.rect.bottom = sprite.rect.top
+                return True
         return False
+    def move_left(self,group):
+        self.rect.move_ip(-TILE_SIZE,0)
+        for sprite in group:
+            if sprite != self and pg.sprite.collide_rect(self,sprite):
+                self.rect.move_ip(TILE_SIZE,0)
+                break
+    def move_right(self,group):
+        self.rect.move_ip(TILE_SIZE,0)
+        for sprite in group:
+            if sprite != self and pg.sprite.collide_rect(self,sprite):
+                self.rect.move_ip(-TILE_SIZE,0)
+                break
 
 class Tetris:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(SCREEN_SIZE)
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(200,100)
+        pg.key.set_repeat(KEY_DELAY,KEY_INTERVAL)
         self.count = 0
+        self.lines = [0] * GRID_HEIGHT
     def draw_grid(self):
         for x in range(0,WIDTH,TILE_SIZE):
             pg.draw.line(self.screen,DARK_GREY,(x,0),(x,HEIGHT))
@@ -51,11 +68,12 @@ class Tetris:
                 if event.key == pg.K_ESCAPE:
                     self.running = False
                 if event.key == pg.K_LEFT:
-                    self.block.rect.move_ip(-TILE_SIZE,0)
+                    self.block.move_left(self.all_sprites)
                 if event.key == pg.K_RIGHT:
-                    self.block.rect.move_ip(TILE_SIZE,0)
+                    self.block.move_right(self.all_sprites)
     def update(self):
-        add_block = self.block.update()
+        add_block = self.block.update(self.all_sprites)
+        self.line_clear()
         if add_block:
             self.count += 1
             self.block = Block(self.count)
@@ -66,6 +84,26 @@ class Tetris:
         for sprite in self.all_sprites:
             self.screen.blit(sprite.surf,sprite.rect)
         pg.display.update()
+    def check_line_clear(self):
+        self.lines = [0] * GRID_HEIGHT
+        for sprite in self.all_sprites:
+            row = sprite.rect.top//TILE_SIZE
+            self.lines[row] += 1
+        if GRID_WIDTH in self.lines:
+            return True
+        return False
+    def line_clear(self):
+        if self.check_line_clear():
+            for row,num in enumerate(self.lines):
+                if num == GRID_WIDTH:
+                    self.remove_row(row)
+    def remove_row(self,row):
+        for sprite in self.all_sprites:
+            cur_row = sprite.rect.top//TILE_SIZE
+            if cur_row == row: 
+                self.all_sprites.remove(sprite)
+            elif cur_row < row: 
+                sprite.rect.y += TILE_SIZE
 
 t = Tetris()
 t.run()
